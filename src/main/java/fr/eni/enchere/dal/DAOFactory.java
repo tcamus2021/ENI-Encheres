7812e1e308ce8768b4e49c4cc799e9727d358d4d
Romane Guézennec <romane.guezennec2021@campus-eni.fr>
2021-07-07 12:32:55 +0200

    FIX - #DAO : implémentation de la factory DAO avec les mocks

--- src/main/java/fr/eni/enchere/dal/DAOFactory.java
+++ src/main/java/fr/eni/enchere/dal/DAOFactory.java
@@ -1,5 +1,59 @@
 package fr.eni.enchere.dal;
 
-public class DAOFactory {
+import fr.eni.enchere.bo.ArticleVendu;
+import fr.eni.enchere.bo.Categorie;
+import fr.eni.enchere.bo.Enchere;
+import fr.eni.enchere.bo.Retrait;
+import fr.eni.enchere.bo.Utilisateur;
+import fr.eni.enchere.dal.mock.MockArticlesVendus;
+import fr.eni.enchere.dal.mock.MockCategories;
+import fr.eni.enchere.dal.mock.MockEncheres;
+import fr.eni.enchere.dal.mock.MockRetrait;
+import fr.eni.enchere.dal.mock.MockUtilisateurs;
 
+/**
+ * 
+ * @author rguezennec2021 & tcamus2021
+ *
+ */
+public class DAOFactory {
+	/**
+	 * Création de DAO article vendu
+	 * @return
+	 */
+	public static DAOEniEnchere<ArticleVendu> getDaoArticlesVendus() {
+		return new MockArticlesVendus();
+	}
+	
+	/**
+	 * Création de DAO categorie
+	 * @return
+	 */
+	public static DAOEniEnchere<Categorie> getDaoCategories() {
+			return new MockCategories();
+	}
+	
+	/**
+	 * Création de DAO Enchere
+	 * @return
+	 */
+	public static DAOEniEnchere<Enchere> getDaoEnchere() {
+		return new MockEncheres();
+	}
+	
+	/**
+	 * Création de DAO retrait
+	 * @return
+	 */
+	public static DAOEniEnchere<Retrait> getDaoRetrait() {
+		return new MockRetrait();
+	}
+	
+	/**
+	 * Création de DAO Utilisateur
+	 * @return
+	 */
+	public static DAOEniEnchere<Utilisateur> getDaoUtilisateurs() {
+		return new MockUtilisateurs();
+	}
 }
