7aa1892302912f3be224b4f20b9a66f0647b4571
Tanguy Camus <tanguy.camus2021@campus-eni.fr>
2021-07-07 10:02:11 +0200

    FIX - #ERR : Creation des excpetions BLL et DAL

---
+++ src/main/java/fr/eni/enchere/dal/DALexception.java
@@ -0,0 +1,18 @@
+package fr.eni.enchere.dal;
+
+/**
+ * 
+ * @author tcamus2021 rguezennec2021
+ *
+ */
+@SuppressWarnings("serial")
+public class DALexception extends Exception {
+	/**
+	 * Constructeur
+	 * 
+	 * @param message
+	 */
+	public DALexception(String message) {
+		super(message);
+	}
+}
