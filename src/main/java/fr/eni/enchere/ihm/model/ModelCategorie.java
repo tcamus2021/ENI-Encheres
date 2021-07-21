4c565a4530a183fd2fbd3970eeaf3edf3254a17d
Romane Guézennec <romane.guezennec2021@campus-eni.fr>
2021-07-21 11:39:38 +0200

    FIX #2004 création des modeles categories, articles et utilisateur

--- src/main/java/fr/eni/enchere/ihm/model/ModelCategorie.java
+++ src/main/java/fr/eni/enchere/ihm/model/ModelCategorie.java
@@ -1,5 +1,55 @@
 package fr.eni.enchere.ihm.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.eni.enchere.bo.Categorie;
+
+/**
+ * 
+ * @author rguezennec2021 & tcamus2021
+ *
+ */
+
 public class ModelCategorie {
+	private List<Categorie> categories = new ArrayList<>();
+
+	/**
+	 * Constructeur vide
+	 */
+	public ModelCategorie() {
+	}
+
+	/*
+	 * Constructeur complet
+	 */
+	public ModelCategorie(List<Categorie> categories) {
+		super();
+		this.categories = categories;
+	}
+
+	/**
+	 * Gatter des categories
+	 * 
+	 * @return
+	 */
+
+	public List<Categorie> getCategories() {
+		return categories;
+	}
+
+	/**
+	 * Setter des articles vendus
+	 * 
+	 * @param categories
+	 */
+	public void setCategories(List<Categorie> categories) {
+		this.categories = categories;
+	}
+
+	@Override
+	public String toString() {
+		return "ModelCategorie [categories=" + categories + "]";
+	}
 
 }
